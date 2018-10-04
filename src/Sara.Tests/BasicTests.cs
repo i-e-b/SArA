b70f543ccaabdfd16852e67116a36ac508b356f5
Iain Ballard <iain.ballard@gocompare.com>
2018-10-04 21:39:42 +0100

    Skeleton

---
+++ src/Sara.Tests/BasicTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+
+namespace Sara.Tests
+{
+    [TestFixture]
+    public class BasicTests
+    {
+        [Test]
+        public void can_allocate_memory_from_a_pool_and_get_a_pointer (){
+            var subject = new Allocator(100, 10000);
+
+            long ptr = subject.Alloc(byteCount: 1000);
+
+            Assert.That(ptr, Is.GreaterThanOrEqualTo(100));
+        }
+
+        [Test]
+        public void a_second_allocation_returns_different_memory (){ }
+
+        [Test]
+        public void can_directly_deallocate_a_pointer (){ }
+
+        [Test]
+        public void deallocating_the_most_recently_allocated_block_returns_the_same_pointer (){ }
+
+        [Test]
+        public void deallocating_an_old_allocation_does_nothing (){ }
+
+        [Test]
+        public void can_add_and_remove_current_referenced_pointers (){ }
+
+        [Test]
+        public void allocating_enough_memory_changes_arena (){ }
+
+        [Test]
+        public void deallocating_everything_in_an_arena_resets_it (){ }
+
+        [Test]
+        public void running_a_scan_when_any_pointers_are_referenced_keeps_the_arena (){ }
+
+        [Test]
+        public void running_a_scan_when_no_pointers_are_referenced_resets_the_arena (){ }
+
+        [Test] // todo: do something better once the rest is working
+        public void requesting_a_block_larger_than_a_single_area_fails (){ }
+    }
+}
---
+++ src/Sara/Allocator.cs
@@ -0,0 +1,25 @@
+﻿namespace Sara
+{
+    public class Allocator
+    {
+        /// <summary>
+        /// Start a new allocator.
+        /// This tracks memory usage, but does not do any physical work
+        /// </summary>
+        /// <param name="start">Start of allocation space (bytes)</param>
+        /// <param name="limit">Maximum memory before an out-of-memory condition is flagged (bytes)</param>
+        public Allocator(long start, long limit)
+        {
+            
+        }
+
+        /// <summary>
+        /// Allocate some bytes. Returns pointer to start of memory.
+        /// Returns negative if failed.
+        /// </summary>
+        public long Alloc(int byteCount)
+        {
+            return 0;
+        }
+    }
+}
