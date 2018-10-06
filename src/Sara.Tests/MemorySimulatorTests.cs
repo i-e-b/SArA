6bfcacdb3d6de094cbe4da253576512e5a73a095
Iain Ballard <iain.ballard@gocompare.com>
2018-10-06 16:49:56 +0100

    Memory simulator

---
+++ src/Sara.Tests/MemorySimulatorTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace Sara.Tests
+{
+    [TestFixture]
+    public class MemorySimulatorTests {
+        [Test]
+        public void reading_and_writing_types ()
+        {
+            var subject = new MemorySimulator(128);
+
+            subject.Write(location: 0, value: (byte)0x88);
+            subject.Write(location: 1, value: (byte)0x77);
+            subject.Write(location: 2, value: (byte)0x00);
+            subject.Write(location: 3, value: (byte)0x00);
+            subject.Write(location: 4, value: (byte)0x00);
+
+            var a = subject.Read<short>(1);
+            Assert.That(a, Is.EqualTo(0x0077)); // due to endian style
+
+            var b = subject.Read<uint>(0);
+            Assert.That(b, Is.EqualTo(0x00007788));
+
+            var c = subject.Read<uint>(1);
+            Assert.That(c, Is.EqualTo(0x00000077));
+        }
+    }
+}
---
+++ src/Sara/MemorySimulator.cs
@@ -0,0 +1,43 @@
+﻿namespace Sara
+{
+    /// <summary>
+    /// Behaves like raw memory access, for simulating embedded systems
+    /// </summary>
+    public class MemorySimulator
+    {
+        private readonly byte[] _data;
+
+        public MemorySimulator(int byteSize)
+        {
+            _data = new byte[byteSize];
+        }
+
+        public static unsafe byte[] ToByteArray<T>(T argument) where T : unmanaged
+        {
+            var size = sizeof(T);
+            var result = new byte[size];
+            byte* p = (byte*)&argument;
+            for (var i = 0; i < size; i++)
+                result[i] = *p++;
+            return result;
+        }
+
+        public unsafe T Read<T>(int location) where T: unmanaged 
+        {
+            T result;
+            fixed (byte* basePtr = &_data[location]) {
+                var tgt = (T*)basePtr;
+                result = *tgt;
+            }
+            return result;
+        }
+
+        public unsafe void Write<T>(int location, T value) where T : unmanaged
+        {
+            fixed (byte* basePtr = &_data[location]) {
+                var tgt = (T*)basePtr;
+                *tgt = value;
+            }
+        }
+    }
+}
